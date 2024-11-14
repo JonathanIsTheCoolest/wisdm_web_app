799d98ab01e55edde2e7be22652ce589b5295920
Jonathan Lascano <jonathanlascano2393@gmail.com>
2024-11-13 18:19:20 -0800

    Uninstall next-view-transitions, Allow photos from graph.facebook.com, Install bad-words

--- next.config.js
+++ next.config.js
@@ -20,6 +20,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'graph.facebook.com',
+      },
     ],
   },
   webpack: (config) => {
